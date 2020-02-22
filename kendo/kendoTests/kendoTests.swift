d3997dcce82b7b8dfcf85fa65bddb9769f600bc0
Yuliang Cai <yuliangc@usc.edu>
2020-02-22 02:56:08 -0800

    ui for launch page, counter page, and remainder page

---
+++ kendo/kendoTests/kendoTests.swift
@@ -0,0 +1,34 @@
+//
+//  kendoTests.swift
+//  kendoTests
+//
+//  Created by Buzz Cai on 2/21/20.
+//  Copyright © 2020 401_group_8. All rights reserved.
+//
+
+import XCTest
+@testable import kendo
+
+class kendoTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
